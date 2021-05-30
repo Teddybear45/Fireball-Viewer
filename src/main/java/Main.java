896b30f087eb94ad85ed209cad4e41a6136bf80a
Ted Guan <tedguan2005@gmail.com>
2021-05-30 00:32:00 -0700

    fireball node analyzing longt and latt

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,45 @@
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String fireballDB = "https://ssd-api.jpl.nasa.gov/fireball.api?req-loc=true";
-        String dataHTML = getHTML(fireballDB);
-        System.out.println(dataHTML);
+        int viewDeg = 10;
 
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        String fireballDB = "https://ssd-api.jpl.nasa.gov/fireball.api?req-loc=true";
+
+        String dataHTML = getHTML(fireballDB);
         JSONObject dataJSON = new JSONObject(dataHTML);
-        System.out.println(dataJSON.getString("fields"));
-        System.out.println(dataJSON.getString("data"));
+        JSONArray fireballDataArr = dataJSON.getJSONArray("data");
+
+        int currentFoundLat, currentFoundLong;
+        int currentFoundHigestEnergy = -1;
+
+        for (int i = 0; i < fireballDataArr.length(); i++) {
+            JSONArray fireballNodeArr = fireballDataArr.getJSONArray(i);
+            System.out.println(fireballNodeArr);
+
+            Double lat = Double.parseDouble((String) fireballNodeArr.get(3));
+
+            System.out.println(lat);
+
+
+
+        }
 
 
 
     }
+
     public static String getHTML(String urlToRead) throws Exception {
         StringBuilder result = new StringBuilder();
         URL url = new URL(urlToRead);
@@ -31,4 +53,14 @@ public class Main {
         }
         return result.toString();
     }
+
+
+    public static boolean checkLat(Double latt, String dir, int deg) {
+
+    }
+
+    public static boolean checkLong(Double longt, String dir, int deg) {
+        
+    }
+
 }
