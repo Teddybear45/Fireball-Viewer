c9c70e18f41030cd903f87a0d43e2928593db9b4
Ted Guan <tedguan2005@gmail.com>
2021-05-30 14:48:23 -0700

    lat and long input checking with data within range and checking energy

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,24 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int viewDeg = 10;
 
+        double currentLat, currentLong;
+        String currentLatDir, currentLongdir;
+
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter Latitude (12.34)");
+        currentLat = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Enter Latitude North or South (N/S)");
+        currentLatDir = scanner.nextLine();
+
+        System.out.println("Enter Longitude (12.34)");
+        currentLong = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Enter Longitude East or West (E/W)");
+        currentLongdir = scanner.nextLine();
+
+        System.out.println("Calculating ---");
 
 
         String fireballDB = "https://ssd-api.jpl.nasa.gov/fireball.api?req-loc=true";
@@ -20,23 +36,39 @@ public class Main {
         String dataHTML = getHTML(fireballDB);
         JSONObject dataJSON = new JSONObject(dataHTML);
         JSONArray fireballDataArr = dataJSON.getJSONArray("data");
+        System.out.println(dataHTML);
 
-        int currentFoundLat, currentFoundLong;
-        int currentFoundHigestEnergy = -1;
+        double currentFoundLat = 0, currentFoundLong = 0;
+        String currentFoundLatDir = null, currentFoundLongDir = null;
+        double currentFoundHigestEnergy = -1.0;
+        boolean foundFireball = false;
 
         for (int i = 0; i < fireballDataArr.length(); i++) {
             JSONArray fireballNodeArr = fireballDataArr.getJSONArray(i);
-            System.out.println(fireballNodeArr);
 
             Double lat = Double.parseDouble((String) fireballNodeArr.get(3));
+            String latDir = (String) fireballNodeArr.get(4);
+            Double longT = Double.parseDouble((String) fireballNodeArr.get(5));
+            String longDir = (String) fireballNodeArr.get(6);
 
-            System.out.println(lat);
-
-
-
+            if (checkLat(lat, latDir, currentLat, currentLatDir, viewDeg) && checkLong(longT, longDir, currentLong, currentLongdir, viewDeg)) {
+                double impactEnergy = Double.parseDouble((String) fireballNodeArr.get(1));
+                if (impactEnergy > currentFoundHigestEnergy) {
+                    foundFireball = true;
+                    currentFoundLat = lat;
+                    currentFoundLatDir = latDir;
+                    currentFoundLong = longT;
+                    currentFoundLongDir = longDir;
+                    currentFoundHigestEnergy = impactEnergy;
+                }
+            }
         }
 
-
+        if (foundFireball) {
+            System.out.println("The nearest fireball to the given location with the highest energy is at: ");
+            System.out.println("Latitude: " + currentFoundLat + " " + currentFoundLatDir);
+            System.out.println("Longitude: " + currentFoundLong + " " + currentFoundLongDir);
+        }
 
     }
 
@@ -54,13 +86,19 @@ public class Main {
         return result.toString();
     }
 
+    //S set to negative arbitrary
+    public static boolean checkLat(Double checkLatt, String checkDir, Double currentLatt, String currentDir, int deg) {
+        currentLatt = (currentDir.equals("S")) ? currentLatt * -1 : currentLatt;
+        checkLatt = (checkDir.equals("S")) ? checkLatt * -1 : checkLatt;
 
-    public static boolean checkLat(Double latt, String dir, int deg) {
-
+        return checkLatt >= (currentLatt - deg) % 180 && checkLatt <= (currentLatt + deg) % 180;
     }
 
-    public static boolean checkLong(Double longt, String dir, int deg) {
-        
-    }
+    //W set to negative arbitrary
+    public static boolean checkLong(Double checkLongt, String checkDir, Double currentLongt, String currentDir, int deg) {
+        currentLongt = (currentDir.equals("W")) ? currentLongt * -1 : currentLongt;
+        checkLongt = (currentDir.equals("W")) ? checkLongt * -1 : checkLongt;
 
+        return checkLongt >= (currentLongt - deg) % 180 && checkLongt <= (currentLongt + deg) % 180;
+    }
 }
